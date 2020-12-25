827ff736430204136cf852d76d2906f61a5c9185
qoterwater <marderics@gmail.com>
2020-12-25 18:54:00 +0300

    поправил

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projects11';
+  title = 'project11';
+
+  sensors: mySensor[] = [
+    {id: 1,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 2,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 3,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 4,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 5,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 6,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 7,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 8,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 9,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+    {id: 10,name:"Детектор", status: Boolean(this.getRandom(1)), show: true},
+  ]
+
+  getRandom(max) {
+    return Math.floor(Math.random() * Math.floor(max+1));
+  }
+  createSensor(sensor_name) {
+    this.sensors.push({id: this.sensors.length+1,name:sensor_name, status: Boolean(this.getRandom(1)), show: true})
+  }
 }
---
+++ src/app/shared/models/det.model.ts
@@ -0,0 +1,6 @@
+class mySensor {
+    id: number;
+    name: string;
+    status: boolean;
+    show: boolean;
+}
